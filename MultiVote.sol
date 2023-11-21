f5d5debf674827ae8a3499d715e85fe29c6bb2e6
LynessGeraldyne <101625487+LynessGeraldyne@users.noreply.github.com>
2023-11-21 07:46:42 +0200

    Update MultiVote.sol

--- MultiVote.sol
+++ MultiVote.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract MultiCandidateVoting {
+    address public admin;
+    mapping(address => bool) public voters;
+    mapping(bytes32 => uint256) public votes;
+    bytes32[] public candidateList;
+
+    event VoteCast(address indexed voter, bytes32 candidate);
+    event CandidateAdded(bytes32 candidate);
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only the admin can call this function");
+        _;
+    }
+
+    modifier onlyVoter() {
+        require(voters[msg.sender], "Only registered voters can participate");
+        _;
+    }
+
+    constructor(address[] memory _voters, bytes32[] memory _initialCandidates) {
+        admin = msg.sender;
+        for (uint256 i = 0; i < _voters.length; i++) {
+            require(_voters[i] != address(0), "Invalid voter address");
+            voters[_voters[i]] = true;
+        }
+
+        for (uint256 i = 0; i < _initialCandidates.length; i++) {
+            addCandidate(_initialCandidates[i]);
+        }
+    }
+
+}
